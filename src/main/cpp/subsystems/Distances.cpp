774407bd9c837e91c91dd663cdfc6fb748f3e95e
akeadey <akeadey2@gmail.com>
2024-02-26 18:00:16 -0500

    apriltag targeting

---
+++ src/main/cpp/subsystems/Distances.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/Distances.h"
+
+Distances::Distances(units::meter_t minDist,
+                     units::meter_t maxDist,
+                     units::meter_t shooterSpeed,
+                     units::meter_t intakeSpeed,
+                     units::meter_t elevatorHeight,
+                     units::angle::radians armHeight) { 
+    
+    m_minDist = minDist;
+    m_maxDist = maxDist;
+    m_shooterSpeed = shooterSpeed;
+    m_intakeSpeed = intakeSpeed;
+    m_elevatorHeight = elevatorHeight;
+}
+
+Distances::Distances() {}
